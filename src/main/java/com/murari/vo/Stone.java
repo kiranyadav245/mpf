ca1ae7943a28cceff5b77814a4baad957bcbea1d
kiranyadav245 <kiranyadav245@gmail.com>
2017-08-01 19:36:28 +0530

    Update Stone.java

--- src/main/java/com/murari/vo/Stone.java
+++ src/main/java/com/murari/vo/Stone.java
@@ -1,8 +1,22 @@
 package com.murari.vo;
 
+import java.sql.Date;
+
 public class Stone {
-	private long id;
+    private long id;
     private String content;
+    private String qualityRange;
+    private long stock; //closing quantity
+    private int unit;   //ton
+    private Date inDate;
+    private String openingQuantity;
+    private String comments; //get by who and which vehicle
+    private long inStone; // capture stone in at the date
+    
+    //stone consume properties
+    private Date consumeDate;
+    private long consumedDate; //how much stone consumed
+    private String operator;
 
     public long getId() {
         return id;
