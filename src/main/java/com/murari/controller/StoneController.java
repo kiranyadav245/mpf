d7f028b4b18bf4dca0857c7bae8c505bea3d5430
kiranyadav245 <kiranyadav245@gmail.com>
2017-08-02 17:48:38 +0530

    Update StoneController.java

--- src/main/java/com/murari/controller/StoneController.java
+++ src/main/java/com/murari/controller/StoneController.java
@@ -1,18 +1,34 @@
 package com.murari.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.murari.vo.QualityRange;
 import com.murari.vo.Stone;
+import com.murari.vo.StoneIn;
 
 @Controller
 public class StoneController {
 	@GetMapping("/stonein")
 	public String stoneInForm(Model model){
-		model.addAttribute("stone", new Stone());
+		Stone stone = new Stone();
+		List<StoneIn> stoneIns = new ArrayList<>();
+		List<QualityRange> qualityRanges = qualityRanges();
+		int i=1;
+		for(QualityRange range : qualityRanges){
+			StoneIn sIn = new StoneIn();
+			sIn.setQualityRange(range);
+			sIn.setStock(25*i++);
+			stoneIns.add(sIn);
+		}
+		stone.setStoneIns(stoneIns);
+		model.addAttribute("stone", stone);
 		return "stone/stoneIn";
 	}
 	
@@ -24,5 +40,16 @@ public class StoneController {
 	@PostMapping("/smsStoneIn")
 	public String smsStoneIn(@ModelAttribute Stone stone){
 		return "stone/smsStoneIn";
+	}
+	
+	
+	public List<QualityRange> qualityRanges(){
+		List<QualityRange> range = new ArrayList<>();
+		range.add(new QualityRange("1","90-100"));
+		range.add(new QualityRange("2","80-90"));
+		range.add(new QualityRange("3","70-80"));
+		range.add(new QualityRange("4","60-70"));
+		range.add(new QualityRange("5","low"));
+		return range;
 	}
 }
